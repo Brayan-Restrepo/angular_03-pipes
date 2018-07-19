c8110807004f26a5d24ae67bff47bca3916a79dd
Brayan-Restrepo <brayan.restrepo@unillanos.edu.co>
2018-07-18 22:40:48 -0500

    pipes

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CapitalizadoPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [ { provide: LOCALE_ID, useValue: 'es' } ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
